55810947fd6d18187f178d8902139602c6b7dbef
Tomi <dobiasz.tamas@gmail.com>
2021-04-14 14:01:28 +0200

    newfodinhome /2021.04.14 13:58

---
+++ NavMenu.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tom
+ * Date: 2021. 02. 27.
+ * Time: 8:50
+ */
+?>
+<div class="menu-container">
+    <div class="menu">
+        <a href="#" class="logo"><i class="fas fa-home"> Fodin-Home Kft</i></a>
+        <ul class="clearfix">
+            <li><a href="index.php">Szöveg</a></li>
+            <li><a href="#">Szöveg</a>
+                <ul>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><img src="images/10.jpg" style="max-width: 240px; height: 200px"></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#">Szöveg</a>
+                <ul>
+                    <li><a href="#">Szöveg</a></li>
+                    <li><a href="#">Szöveg</a></li>
+                    <li><a href="#">Szöveg</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Szöveg</a>
+                <ul>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                            <li><a href="#">Szöveg</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Szöveg</a>
+                        <ul>
+                            <li><img src="images/10.jpg" style="max-width: 250px; height: 200px"></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
